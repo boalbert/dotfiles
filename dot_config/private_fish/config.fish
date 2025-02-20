644666c515e03f946c2b453e6318f5f8cdff5b8b
Albert Andersson <albert.andersson@callistaenterprise.se>
2025-02-20 20:11:37 +0100

    Sync

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -22,8 +22,16 @@ if status is-interactive
     abbr --add dsa docker ps
 
     ## fish config
-    abbr --add cfgfish zed ~/.config/fish/config.fish
-    abbr --add cfgtmux zed ~/.tmux.conf
+    abbr --add cfgfish chezmoi edit ~/.config/fish/config.fish
+    abbr --add cfggho chezmoi edit ~/.config/ghostty/config
+
+    ## Brandon
+    abbr --add logger cd ~/dev/repos/brandon/brandon-camel-logger
+    abbr --add router cd ~/dev/repos/brandon/brandon-camel-router
+    abbr --add webshop cd ~/dev/repos/brandon/brandon-camel-webshop-adapter
+    abbr --add common cd ~/dev/repos/brandon/brandon-common-camel
+    abbr --add docs cd ~/dev/repos/brandon/int-docs
+    abbr --add warehouse cd ~/dev/repos/brandon/brandon-camel-warehouse-sweden-adapter
 
     ## replace cat with bat
     abbr --add cat bat
@@ -31,8 +39,8 @@ if status is-interactive
     abbr --add sourcefish source ~/.config/fish/config.fish
 
     # Change default location of vim config
-    set -Ux VISUAL "vim"
-    set -Ux EDITOR "vim"
+    # set -Ux VISUAL "vim"
+    # set -Ux EDITOR "vim"
     set -x VIMINIT 'source ~/.config/vim/.vimrc'
 
     # Open scratch notes in vim
@@ -46,4 +54,13 @@ if status is-interactive
     # Adding a comment to trigger chezmoi
     # Start ssh agent
     set SSH_AUTH_SOCK /var/folders/5l/5khp6mf95qqg27fcwxy3hn480000gn/T//ssh-petcZlkXvLcc/agent.7616
+
+    # Set Zed as default editor
+    set -Ux VISUAL "zed --wait"
+    set -gx EDITOR "zed --wait"
+
+    # Set up fzf key binding: https://github.com/junegunn/fzf?tab=readme-ov-file#setting-up-shell-integration
+    fzf --fish | source
+
+
 end
